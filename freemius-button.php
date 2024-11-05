51539b525104d24c64bd2067026ebfa6eacfa204
Xaver <xaver@everpress.co>
2024-11-05 14:06:46 +0100

    load editor scripts only in the backend

--- freemius-button.php
+++ freemius-button.php
@@ -18,16 +18,20 @@ if ( ! defined( 'ABSPATH' ) ) {
 }
 
 
-add_action( 'enqueue_block_assets', __NAMESPACE__ . '\block_script_styles', 1 );
+\add_action( 'enqueue_block_assets', __NAMESPACE__ . '\block_script_styles', 1 );
 function block_script_styles() {
 
-	$plugin_dir = plugin_dir_path( __FILE__ );
-	$plugin_url = plugin_dir_url( __FILE__ );
+	if ( ! is_admin() ) {
+		return;
+	}
+
+	$plugin_dir = \plugin_dir_path( __FILE__ );
+	$plugin_url = \plugin_dir_url( __FILE__ );
 
 	// load from assets.php
 	$dependecied = include $plugin_dir . 'build/editor.asset.php';
-	wp_enqueue_script( 'freemius-button', $plugin_url . 'build/editor.js', $dependecied['dependencies'], $dependecied['version'], true );
-	wp_enqueue_style( 'freemius-button', $plugin_url . 'build/editor.css', array(), $dependecied['version'] );
+	\wp_enqueue_script( 'freemius-button', $plugin_url . 'build/editor.js', $dependecied['dependencies'], $dependecied['version'], true );
+	\wp_enqueue_style( 'freemius-button', $plugin_url . 'build/editor.css', array(), $dependecied['version'] );
 }
 
 
@@ -50,21 +54,21 @@ function render_button( $block_content, $block, $instance ) {
 	 *
 	 * @param array $data The data that will be passed to the Freemius checkout.
 	 */
-	$data = apply_filters( 'freemius_button_data', $block['attrs']['freemius'] );
+	$data = \apply_filters( 'freemius_button_data', $block['attrs']['freemius'] );
 
 	$extra = '';
 	// $extra  .= '<pre type="application/json">' . json_encode( $data ) . '</pre>';
 	$extra .= '<script type="application/json" class="freemius-button-data">' . json_encode( $data ) . '</script>';
 
-	$plugin_dir = plugin_dir_path( __FILE__ );
-	$plugin_url = plugin_dir_url( __FILE__ );
+	$plugin_dir = \plugin_dir_path( __FILE__ );
+	$plugin_url = \plugin_dir_url( __FILE__ );
 
-	wp_enqueue_script( 'freemius-button-checkout', 'https://checkout.freemius.com/checkout.min.js', array('jquery'), '0.1.0', true );
+	\wp_enqueue_script( 'freemius-button-checkout', 'https://checkout.freemius.com/js/v1/', array(), '0.1.0', true );
 
 	// load from assets.php
 	$dependecied = include $plugin_dir . 'build/view.asset.php';
-	wp_enqueue_script( 'freemius-button-frontend', $plugin_url . 'build/view.js', $dependecied['dependencies'], $dependecied['version'], true );
-	wp_enqueue_style( 'freemius-button-frontend', $plugin_url . 'build/view.css', array(), $dependecied['version'] );
+	\wp_enqueue_script( 'freemius-button-frontend', $plugin_url . 'build/view.js', $dependecied['dependencies'], $dependecied['version'], true );
+	\wp_enqueue_style( 'freemius-button-frontend', $plugin_url . 'build/view.css', array(), $dependecied['version'] );
 
 	return $extra . $block_content;
 }
